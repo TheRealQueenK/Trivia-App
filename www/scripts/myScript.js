47ec805a350efc85597f461a22b3383ec7af7dc5
TheRealQueenK <kelsieww@gmail.com>
2016-06-28 00:45:30 +0900

    Project's Initial Commit

---
+++ www/scripts/myScript.js
@@ -0,0 +1,19 @@
+function Incorrect(){
+   //grab the element we want to manipulate
+   var wrongAnswer = document.getElementById("wrong1");
+   wrongAnswer.style.color = "red";
+}    
+function Incorrect2(){
+   var wrongAnswer = document.getElementById("wrong2");
+   wrongAnswer.style.color = "red";
+}  
+function Incorrect3(){
+   var wrongAnswer = document.getElementById("wrong3");
+   wrongAnswer.style.color = "red";
+} 
+
+function Correct(){
+   //grab the element we want to manipulate
+   var rightAnswer = document.getElementById("right1");
+   rightAnswer.style.color = "green";
+}  
