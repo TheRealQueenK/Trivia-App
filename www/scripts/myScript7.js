69c086bf7ebb3f1760a8e71cff61794e0d07b812
TheRealQueenK <kelsieww@gmail.com>
2016-07-01 16:30:59 +0900

    created button back to start

---
+++ www/scripts/myScript7.js
@@ -0,0 +1,44 @@
+var points = 0;
+var current;
+function ScoreUp(){
+    current = 10;
+    points = points + current;
+    document.getElementById("scoreCard").innerHTML = "Score: " + points;
+    
+}
+
+function ScoreDown(){
+    current = 5;
+    points = points - current;
+    document.getElementById("scoreCard").innerHTML = "Score: " + points;
+}
+function Navigate(pageName){
+    window.location = pageName;
+    
+    
+}
+function Incorrect(){
+   //grab the element we want to manipulate
+   var wrongAnswer = document.getElementById("wrong1");
+   wrongAnswer.style.color = "red";
+//   ScoreDown();
+}    
+function Incorrect2(){
+   var wrongAnswer = document.getElementById("wrong2");
+   wrongAnswer.style.color = "red";
+//   ScoreDown();
+}  
+function Incorrect3(){
+   var wrongAnswer = document.getElementById("wrong3");
+   wrongAnswer.style.color = "red";
+//   ScoreDown();
+} 
+
+
+function Correct(){
+   //grab the element we want to manipulate
+   var rightAnswer = document.getElementById("right1");
+   rightAnswer.style.color = "green";
+//   ScoreUp();
+   window.setTimeout(Navigate("index.html"), 1000)
+}  
